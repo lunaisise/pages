32b126662ef58861687f1f422d9f5efb0c7f252b
lunaisise <lunaisise@hotmail.co.jp>
2020-04-25 18:45:56 +0900

    jsを変更

--- qr/js/index.js
+++ qr/js/index.js
@@ -1,60 +1,61 @@
-var isiPhoneXSeries = () => {
-    var isiPhone = /iPhone/.test(window.navigator.userAgent);
+$(() => {
+    const isiPhoneXSeries = (() => {
+        const isiPhone = /iPhone/.test(window.navigator.userAgent);
 
-    var isiPhoneXorXs = window.devicePixelRatio === 3 && (window.screen.width === 375 || window.screen.height === 375);
-    var isiPhoneXsMax = window.devicePixelRatio === 3 && (window.screen.width === 414 || window.screen.height === 414);
-    var isiPhoneXR = window.devicePixelRatio === 2 && (window.screen.width === 414 || window.screen.height === 414);
+        const isiPhoneXorXs = window.devicePixelRatio === 3 && (window.screen.width === 375 || window.screen.height === 375);
+        const isiPhoneXsMax = window.devicePixelRatio === 3 && (window.screen.width === 414 || window.screen.height === 414);
+        const isiPhoneXR = window.devicePixelRatio === 2 && (window.screen.width === 414 || window.screen.height === 414);
 
-    return isiPhone && (isiPhoneXorXs || isiPhoneXsMax || isiPhoneXR);
-};
+        return isiPhone && (isiPhoneXorXs || isiPhoneXsMax || isiPhoneXR);
+    })();
 
-function getCookie() {
-    var qrText = $.cookie('qr-text');
-    if (qrText !== undefined && qrText.length !== 0) {
-        $('#qr-setting').text(qrText);
-        $('#qr-text').val(qrText);
+    function getCookie() {
+        const qrText = $.cookie('qr-text');
+        if (qrText !== undefined && qrText.length !== 0) {
+            $('#qr-setting').text(qrText);
+            $('#qr-text').val(qrText);
+        }
+
+        const qrSize = $.cookie('qr-size');
+        if (qrSize !== undefined) {
+            $('#qr-size').val(qrSize);
+            $('#qr-range').val(qrSize);
+        }
     }
 
-    var qrSize = $.cookie('qr-size');
-    if (qrSize !== undefined) {
-        $('#qr-size').val(qrSize);
-        $('#qr-range').val(qrSize);
+    function setCookie() {
+        $.cookie('qr-text', $('#qr-text').val(), {expires: 7});
+        $.cookie('qr-size', $('#qr-size').val(), {expires: 7});
     }
-}
 
-function setCookie() {
-    $.cookie('qr-text', $('#qr-text').val(), {expires: 7});
-    $.cookie('qr-size', $('#qr-size').val(), {expires: 7});
-}
-
-function makeQrCode() {
-    var qrText = Encoding.convert($('#qr-text').val(), 'SJIS');
-    var qrSize = $('#qr-size').val();
-    $('#qrcode').empty();
-    $('#qrcode').qrcode({
-        height: qrSize,
-        width: qrSize,
-        text: qrText
-    });
-    var padding = qrSize / 25 * 4;
-    $('#qrcode').css('padding', padding + 'px');
-    $('#qrcode').css('padding-bottom', padding - 5 + 'px');
-}
-
-function initRange() {
-    var bodyWidth = $('body').innerWidth();
-    var maxWidth = bodyWidth * 25 / 34;
-    $('#qr-size').attr('max', maxWidth);
-    $('#qr-range').attr('max', maxWidth);
-    var rangeVal = $('#qr-size').val();
-    if (maxWidth < rangeVal) {
-        $('#qr-size').val(maxWidth);
-        $('#qr-range').val(maxWidth);
+    function makeQrCode() {
+        const qrText = Encoding.convert($('#qr-text').val(), 'SJIS');
+        const qrSize = $('#qr-size').val();
+        $('#qrcode').empty();
+        $('#qrcode').qrcode({
+            height: qrSize,
+            width: qrSize,
+            text: qrText
+        });
+        const padding = qrSize / 25 * 4;
+        $('#qrcode').css('padding', `${padding}px`);
+        const paddingBottom = padding - 5;
+        $('#qrcode').css('padding-bottom', `${paddingBottom}px`);
     }
-}
 
-$(function() {
-    if (isiPhoneXSeries()) {
+    function initRange() {
+        const bodyWidth = $('body').innerWidth();
+        const maxWidth = bodyWidth * 25 / 34;
+        $('#qr-size').attr('max', maxWidth);
+        $('#qr-range').attr('max', maxWidth);
+        const rangeVal = $('#qr-size').val();
+        if (maxWidth < rangeVal) {
+            $('#qr-size').val(maxWidth);
+            $('#qr-range').val(maxWidth);
+        }
+    }
+
+    if (isiPhoneXSeries) {
         $('#inputs').css('bottom', '34pt');
     }
 
@@ -62,37 +63,37 @@ $(function() {
 
     initRange();
     makeQrCode();
-});
 
-$(window).on('resize', function() {
-    initRange();
-    makeQrCode();
-});
+    $(window).on('resize', function() {
+        initRange();
+        makeQrCode();
+    });
 
-$('#knob').on('click', function() {
-    if ($(this).data('status') == 'open') {
-        $('#inputs').css('height', 'calc(2rem + 1px)');
-        $(this).data('status', 'close').text('∧');
-    } else {
-        $('#inputs').css('height', '190px');
-        $(this).data('status', 'open').text('∨');
-    }
-});
+    $('#knob').on('click', function() {
+        if ($(this).data('status') == 'open') {
+            $('#inputs').css('height', 'calc(2rem + 1px)');
+            $(this).data('status', 'close').text('∧');
+        } else {
+            $('#inputs').css('height', '190px');
+            $(this).data('status', 'open').text('∨');
+        }
+    });
 
-$('#qr-text').on('change, keyup', function() {
-    $('#qr-setting').text($(this).val());
-    makeQrCode();
-    setCookie();
-});
+    $('#qr-text').on('change, keyup', function() {
+        $('#qr-setting').text($(this).val());
+        makeQrCode();
+        setCookie();
+    });
 
-$('#qr-size').on('change, input', function() {
-    $('#qr-range').val($(this).val());
-    makeQrCode();
-    setCookie();
-});
+    $('#qr-size').on('change, input', function() {
+        $('#qr-range').val($(this).val());
+        makeQrCode();
+        setCookie();
+    });
 
-$('#qr-range').on('change, input', function() {
-    $('#qr-size').val($(this).val());
-    makeQrCode();
-    setCookie();
+    $('#qr-range').on('change, input', function() {
+        $('#qr-size').val($(this).val());
+        makeQrCode();
+        setCookie();
+    });
 });
